2f5fb188deb25d8830ac5fecf86471f8924e7208
M4GWaker <67283309+M4GWaker@users.noreply.github.com>
2020-12-28 21:40:24 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.options import Options
+
+class bcolors:
+    SUCCES = '\033[94m'
+    ERROR = '\033[93m'
+
+chrome_options = Options()
+chrome_options.add_argument(
+     '--user-agent=Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0. 3945.79 Mobile Safari/537.36')
+browser = webdriver.Chrome("chromedriver.exe", options=chrome_options)
+url = "https://haveibeenpwned.com/unifiedsearch/"
+fichier = open('email.txt', "r")
+ligne = fichier.readlines()
+fn = open("succes.txt", "w+")
+
+for line in ligne:
+    time.sleep(3)
+    goodurl = url + line.strip()
+    browser.get(goodurl)
+    try:
+        browser.find_element_by_id("main-frame-error")
+        fn.write(line.strip()  + '\n')
+        print(bcolors.SUCCES + "| WakerChecker | " + line.strip() + " Est Pas Publique !")
+    except:
+        print(bcolors.ERROR + "| WakerChecker | Admin" + line.strip() + " Est Publique !")
+
+fn.close()
+fichier.close()
+browser.close()
